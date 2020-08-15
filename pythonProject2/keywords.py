87b173693e47d8c165f0351352d7fd8bd5b68976
renzo305 <62869917+renzo305@users.noreply.github.com>
2020-08-15 22:47:32 +0800

    Add files via upload

---
+++ pythonProject2/keywords.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+class Keywords():
+    def setUp(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--incognito")
+        self.driver = webdriver.Chrome(executable_path="D:\Chromedriver\chromedriver.exe", options=self.options)
+
+    def inputText(self, fieldXpath):
+        time.sleep(5)
+        self.driver.find_element_by_xpath(fieldXpath).send_keys("johnemail@test.com")
+
+    def clickBtn(self, fieldXpath):
+        time.sleep(5)
+        self.driver.find_element_by_xpath(fieldXpath).click()
+        time.sleep(5)
+
+    def checkElementPresence(self, fieldXpath):
+        self.driver.find_element_by_xpath(fieldXpath)
---
+++ pythonProject2/testpage.py
@@ -0,0 +1,29 @@
+import unittest
+from locators import *
+from selenium import webdriver
+import chromedriver_binary
+import time
+from keywords import Keywords
+
+
+
+class createEmail(unittest.TestCase, Keywords):
+
+
+    def setUp(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--incognito")
+        self.driver = webdriver.Chrome(executable_path="D:\Chromedriver\chromedriver.exe", options=self.options)
+        self.driver.maximize_window()
+
+
+    def test_email_reg(self):
+        self.driver.get("http://automationpractice.com/")
+        self.clickBtn(Login_Page.login_tab)
+        self.checkElementPresence(Login_Page.login_page_check)
+        self.inputText(Login_Page.login_email_box)
+        self.clickBtn(Login_Page.login_email_submit)
+
+
+    def tearDown(self):
+        self.driver.close()
